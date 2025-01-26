2504def5de2fd15fd3212c7ea22f95f0f701b53b
Austin Kong <konghoitec@gmail.com>
2025-01-26 20:25:23 +0800

    Main menu and level select

--- Assets/Scenes/Minigames/MoleculePool/Scripts/Molecule.cs
+++ Assets/Scenes/Minigames/MoleculePool/Scripts/Molecule.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Molecule : MonoBehaviour
 {
+  [SerializeField] private Sprite[] sprites;
+
   private bool isStatic = true;
   private const float VELOCITY = 16f;
+  private int bounces = 0;
+
+  private void Start() {
+    GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+  }
 
   private void Update() {
     if (!isStatic) {
@@ -12,10 +19,18 @@ public class Molecule : MonoBehaviour
   }
 
   private void OnTriggerEnter2D(Collider2D o) {
+    if (isStatic) return;
+
     if (o.TryGetComponent<Molecule>(out Molecule other)) {
       isStatic = true;
       transform.position = MoleculeManager.Instance.AlignToGrid(transform.position);
       MoleculeManager.Instance.Merge(this, other);
+    } else {
+      if (bounces > 0) Destroy(gameObject);
+      Vector3 currEulers = transform.rotation.eulerAngles;
+      transform.rotation = Quaternion.Euler(0, 0, -currEulers.z);
+      transform.Translate(Vector3.up * VELOCITY * Time.deltaTime);
+      bounces++;
     }
   }
 
@@ -23,5 +38,7 @@ public class Molecule : MonoBehaviour
     Destroy(gameObject);
   }
 
-  public void Shoot() => isStatic = false;
+  public void Shoot() {
+    isStatic = false;
+  }
 }
