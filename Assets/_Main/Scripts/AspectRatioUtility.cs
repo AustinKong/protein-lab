5b9fa59075743e9d45fa2a219bd389fd74ca1ed9
GainQh <gqh0712@gmail.com>
2025-08-25 15:09:48 +0800

    Change tutorial video & build as web game

--- Assets/_Main/Scripts/AspectRatioUtility.cs
+++ Assets/_Main/Scripts/AspectRatioUtility.cs
@@ -44,3 +44,127 @@ public class AspectRatioUtility : MonoBehaviour
 
   }
 }
+
+/*using UnityEngine;
+
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Camera))]
+public class AspectRatioUtility : MonoBehaviour
+{
+    public enum FitMode { ExpandToFit, Letterbox }
+
+    public float targetWidth = 16f;
+    public float targetHeight = 9f;
+
+    public float baseOrthoSize = 5f;
+
+    [Range(1f, 179f)]
+    public float baseVerticalFOV = 60f;
+
+    public FitMode fitMode = FitMode.ExpandToFit;
+
+    public bool expandPerspectiveFOV = true;
+
+    Camera cam;
+    int lastW, lastH;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        Apply();
+    }
+
+    void OnEnable()
+    {
+        Apply();
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        if (!cam) cam = GetComponent<Camera>();
+        Apply();
+    }
+#endif
+
+    void Update()
+    {
+        if (Screen.width != lastW || Screen.height != lastH)
+        {
+            ApplyNextFrame();
+        }
+    }
+
+    void ApplyNextFrame()
+    {
+        lastW = Screen.width; lastH = Screen.height;
+        StartCoroutine(_ApplyNextFrame());
+    }
+
+    System.Collections.IEnumerator _ApplyNextFrame()
+    {
+        yield return null;
+        Apply();
+        Canvas.ForceUpdateCanvases();
+    }
+
+    public void Apply()
+    {
+        if (!cam) cam = GetComponent<Camera>();
+
+        float targetAspect = targetWidth / Mathf.Max(0.0001f, targetHeight);
+        float windowAspect = (float)Screen.width / Mathf.Max(1, (float)Screen.height);
+
+        if (fitMode == FitMode.ExpandToFit)
+        {
+            cam.rect = new Rect(0f, 0f, 1f, 1f);
+
+            if (cam.orthographic)
+            {
+                if (windowAspect < targetAspect)
+                {
+                    float scale = targetAspect / windowAspect; 
+                    cam.orthographicSize = baseOrthoSize * scale;
+                }
+                else
+                {
+                    cam.orthographicSize = baseOrthoSize;
+                }
+            }
+            else
+            {
+                if (expandPerspectiveFOV && windowAspect < targetAspect)
+                {
+                    float baseRad = baseVerticalFOV * Mathf.Deg2Rad * 0.5f;
+                    float newRad = Mathf.Atan(Mathf.Tan(baseRad) * (targetAspect / windowAspect));
+                    cam.fieldOfView = Mathf.Clamp(newRad * 2f * Mathf.Rad2Deg, 1f, 179f);
+                }
+                else
+                {
+                    cam.fieldOfView = baseVerticalFOV;
+                }
+
+                cam.rect = new Rect(0f, 0f, 1f, 1f);
+                cam.ResetAspect(); 
+            }
+        }
+        else 
+        {
+            float scaleHeight = windowAspect / targetAspect;
+            if (scaleHeight < 1f)
+            {
+                cam.rect = new Rect(0f, (1f - scaleHeight) * 0.5f, 1f, scaleHeight);
+            }
+            else
+            {
+                float scaleWidth = 1f / scaleHeight;
+                cam.rect = new Rect((1f - scaleWidth) * 0.5f, 0f, scaleWidth, 1f);
+            }
+
+            if (cam.orthographic) cam.orthographicSize = baseOrthoSize;
+            else cam.fieldOfView = baseVerticalFOV;
+        }
+    }
+}*/
+
